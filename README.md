f6e03a0955be89e5380a587981020bc7eb64bc86
sudo-dax <dax@scholtz.com.au>
2020-09-12 10:49:28 +1000

    Completed pipeline to SQLite database and updated Documentation

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Scrapy_example_quotes
+This code shows how to scrape quotes from a website and save them to an SQLite database.
+It will serve as an example to adapt to other uses. 
 
+./quotes_spider.py - Contains CSS selectors
+./pipelines.py - Creation & Connection to SQLite Database
+./myquotes.db - empty database into which the quotes table will be created and populated. 
+./test - Basic example of sqlite connection 
+./test2 - Example of sqlite connection fed from array of seed data
 
-## To run crawler type
+## Install Dependencies:
+- [Python 3.8.5](https://www.python.org/downloads/release/python-385/)
+- [Scrapy 2.3.0](https://docs.scrapy.org/en/latest/intro/install.html)
+
+## To run crawler & populate database type
 
 ``` 
 scrapy crawl quotes
--- quotetutorial/pipelines.py
+++ quotetutorial/pipelines.py
@@ -6,8 +6,38 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
+class QuotetutorialPipeline(object):
 
-class QuotetutorialPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+        # self.curr = self.connection.cursor()
+        # self.process_item()
+        # self.store_db()
+        
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("myquotes.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        # self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""CREATE TABLE IF NOT EXISTS quotes_tb(
+                            title text,
+                            author text,
+                            tag text
+                            )""")
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute("""INSERT INTO quotes_tb VALUES (?,?,?)""", (item['title'][0], item['author'][0], item['tag'][0]))
+        self.conn.commit()
+
+        ## Close to only recieve a single item
+        # self.curr.close()
+        # self.conn.close()
